1f141fe4188a246702a0ccf39f267364791ce792
Gabriel Abiera <gabiera@stratpoint.com>
2025-08-18 00:03:41 +0800

    developed the frontend of priest directory using quasar

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class=" bg-white text-primary" height-hint="98" >
+      <q-toolbar>
+        <div class="flex items-center q-gutter-md">
+         
+         
+        <q-toolbar-title style="color: #000075; font-weight: bold; font-size: 3rem; flex-direction: top;">
+
+            <!-- <img src="https://catholink.ph/wp-content/uploads/2021/11/Diocese-of-Novaliches.png" alt="Diocese of Novaliches" style="width: 50px; height: 50px;"> -->
+          DIOCESE OF NOVALICHES
+        </q-toolbar-title>
+        </div>
+      </q-toolbar>
+      
+
+      <q-tabs align="left" style="background-color: #000047; color: white;">
+        <q-route-tab to="/page1" label="SEARCH" />
+        <q-route-tab to="/page2" label="PRIESTS" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, } from 'vue'
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  methods: {
+  },
+})
+</script>
---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="column items-center q-gutter-md">
+      <h3 class="text-h3">Welcome to the Diocese of Novaliches - Priest Directory</h3>
+      <!-- <img
+        alt="Diocese of Novaliches"
+        src="https://catholink.ph/wp-content/uploads/2021/11/Diocese-of-Novaliches.png"
+        style="width: 200px; height: 200px;"
+      /> -->
+      <q-input
+        v-model="search"
+        debounce="1000"
+        filled
+        placeholder="Search a Priest"
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </div>
+  </q-page>
+</template>
